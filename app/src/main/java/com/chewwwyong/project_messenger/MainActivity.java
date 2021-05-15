117b5b7c64dab3c72651abd64123400dea0e7215
chewwwyong <chewwwyong@gmail.com>
2021-05-15 15:53:17 +0800

    Initial commit

---
+++ app/src/main/java/com/chewwwyong/project_messenger/MainActivity.java
@@ -0,0 +1,249 @@
+package com.chewwwyong.project_messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = "AiotMqtt";
+    /* 设备三元组信息 */
+    final private String PRODUCTKEY = "a11xsrWmW14";
+    final private String DEVICENAME = "paho_android";
+    final private String DEVICESECRET = "tLMT9QWD36U2SArglGqcHCDK9rK9nOrA";
+
+    /* 自动Topic, 用于上报消息 */
+    private String PUB_TOPIC = "who";
+    /* 自动Topic, 用于接受消息 */
+    private String SUB_TOPIC = "diderwei";
+
+
+    final String host = "tcp://test.mosquitto.org:1883";
+    private String clientId = "project_Messenger_Subcribe";
+    //private String userName = "chewwwyong";
+    //private String passWord = "123456";
+
+    TextView txv_Message_box;
+    EditText edt_getText;
+    String who;
+    ArrayList<String> addFriend = new ArrayList<>();
+    MqttAndroidClient mqttAndroidClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txv_Message_box = findViewById(R.id.txv_Message_box);
+        edt_getText = findViewById(R.id.edt_getText);
+
+        Intent it = getIntent();
+        who = it.getStringExtra("who");
+        addFriend = it.getStringArrayListExtra("FriendList");
+        PUB_TOPIC = who;
+
+        /* 获取Mqtt建连信息clientId, username, password */
+        AiotMqttOption aiotMqttOption = new AiotMqttOption().getMqttOption(PRODUCTKEY, DEVICENAME, DEVICESECRET);
+        if (aiotMqttOption == null) {
+            Log.e(TAG, "device info error");
+        } else {
+            clientId = aiotMqttOption.getClientId();
+            //userName = aiotMqttOption.getUsername();
+            //passWord = aiotMqttOption.getPassword();
+        }
+
+        /* 创建MqttConnectOptions对象并配置username和password */
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        //mqttConnectOptions.setUserName(userName);
+        //mqttConnectOptions.setPassword(passWord.toCharArray());
+
+
+        /* 创建MqttAndroidClient对象, 并设置回调接口 */
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), host, clientId);
+        mqttAndroidClient.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.i(TAG, "connection lost");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.i(TAG, "topic: " + topic + ", msg: " + new String(message.getPayload()));
+                Toast.makeText(MainActivity.this,"Topic: " + topic + "\n msg: \n" + new String(message.getPayload()),Toast.LENGTH_SHORT).show();
+                txv_Message_box.setText(
+                        txv_Message_box.getText() +
+                        "\n" + who + " : " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                Log.i(TAG, "msg delivered");
+            }
+        });
+
+        /* Mqtt建连 */
+        try {
+            mqttAndroidClient.connect(mqttConnectOptions,null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.i(TAG, "connect succeed");
+                    //    Toast.makeText(MainActivity.this,"connect succeed",Toast.LENGTH_SHORT).show();
+
+                    for(int i=0;i<addFriend.size();i++) {
+                        SUB_TOPIC = addFriend.get(i);
+                        subscribeTopic(SUB_TOPIC);
+                    }
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.i(TAG, "connect failed");
+                }
+            });
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+
+
+        /* 通过按键发布消息 */
+        Button pubButton = findViewById(R.id.publish);
+        pubButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publishMessage(edt_getText.getText().toString());
+                txv_Message_box.setText(txv_Message_box.getText().toString() +
+                        "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t" + edt_getText.getText().toString());
+            }
+        });
+    }
+
+    /**
+     * 订阅特定的主题
+     * @param topic mqtt主题
+     */
+    public void subscribeTopic(String topic) {
+        try {
+            mqttAndroidClient.subscribe(topic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.i(TAG, "subscribed succeed");
+                    //   Toast.makeText(MainActivity.this,"subscribed succeed",Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.i(TAG, "subscribed failed");
+                }
+            });
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 向默认的主题/user/update发布消息
+     * @param payload 消息载荷
+     */
+    public void publishMessage(String payload) {
+        try {
+            if (mqttAndroidClient.isConnected() == false) {
+                mqttAndroidClient.connect();
+            }
+
+            MqttMessage message = new MqttMessage();
+            message.setPayload(payload.getBytes());
+            message.setQos(0);
+            mqttAndroidClient.publish(PUB_TOPIC, message,null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.i(TAG, "publish succeed!");
+                    //   Toast.makeText(MainActivity.this,"publish succeed",Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.i(TAG, "publish failed!");
+                }
+            });
+        } catch (MqttException e) {
+            Log.e(TAG, e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * MQTT建连选项类，输入设备三元组productKey, deviceName和deviceSecret, 生成Mqtt建连参数clientId，username和password.
+     */
+    class AiotMqttOption {
+        private String username = "";
+        private String password = "";
+        private String clientId = "";
+
+        public String getUsername() { return this.username;}
+        public String getPassword() { return this.password;}
+        public String getClientId() { return this.clientId;}
+
+        /**
+         * 获取Mqtt建连选项对象
+         * @param productKey 产品秘钥
+         * @param deviceName 设备名称
+         * @param deviceSecret 设备机密
+         * @return AiotMqttOption对象或者NULL
+         */
+        public AiotMqttOption getMqttOption(String productKey, String deviceName, String deviceSecret) {
+            if (productKey == null || deviceName == null || deviceSecret == null) {
+                return null;
+            }
+
+            try {
+                String timestamp = Long.toString(System.currentTimeMillis());
+
+                // clientId
+                this.clientId = productKey + "." + deviceName + "|timestamp=" + timestamp +
+                        ",_v=paho-android-1.0.0,securemode=2,signmethod=hmacsha256|";
+
+                // userName
+                this.username = deviceName + "&" + productKey;
+
+                // password
+                String macSrc = "clientId" + productKey + "." + deviceName + "deviceName" +
+                        deviceName + "productKey" + productKey + "timestamp" + timestamp;
+                String algorithm = "HmacSHA256";
+                Mac mac = Mac.getInstance(algorithm);
+                SecretKeySpec secretKeySpec = new SecretKeySpec(deviceSecret.getBytes(), algorithm);
+                mac.init(secretKeySpec);
+                byte[] macRes = mac.doFinal(macSrc.getBytes());
+                password = String.format("%064x", new BigInteger(1, macRes));
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            return this;
+        }
+    }
+}
---
+++ app/src/main/java/com/chewwwyong/project_messenger/choose_who.java
@@ -0,0 +1,50 @@
+package com.chewwwyong.project_messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class choose_who extends AppCompatActivity {
+
+    TextView txv_test,txv_test2;
+    EditText edt_addFriend;
+    Button btn_subcribe;
+    ArrayList<String> addFriend = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_who);
+
+        txv_test = findViewById(R.id.txv_test);
+        txv_test2 = findViewById(R.id.txv_test2);
+        edt_addFriend = findViewById(R.id.edt_addFriend);
+        btn_subcribe  = findViewById(R.id.btn_subcribe);
+
+        btn_subcribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addFriend.add(edt_addFriend.getText().toString());
+                txv_test.setText(edt_addFriend.getText().toString());
+            }
+        });
+
+        txv_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(choose_who.this,MainActivity.class);
+                it.putExtra("who", "bigIN_GG");
+                it.putStringArrayListExtra("FriendList", addFriend);
+                startActivity(it);
+
+            }
+        });
+    }
+}
